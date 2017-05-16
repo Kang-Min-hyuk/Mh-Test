0e2cd24e9382ea69f4adb36ba3fda33f36069d38
Kang-Min-hyuk <khukang1@gmail.com>
2017-05-16 09:06:18 +0900

    안드로이드 I/O HTTP

--- File I-O/MainActivity.java
+++ File I-O/MainActivity.java
@@ -1,4 +1,4 @@
-package com.test.exandroidsocket_0515;
+package com.test.exhttp_0515;
 
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,82 +8,96 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    EditText editText, editText2;
+    EditText editText;
+    TextView txtMsg;
 
-    Handler handler =new Handler();
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
         editText = (EditText) findViewById(R.id.editText);
-        editText2 = (EditText) findViewById(R.id.editText2);
+        txtMsg = (TextView) findViewById(R.id.txtMsg);
 
-        Button button = (Button) findViewById(R.id.btnConnect);
+        Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String addr = editText.getText().toString();
+                String strUrl = editText.getText().toString();
 
-                Thread thread =new Thread(new ConnectRunnable(addr));
+                Thread thread = new Thread(new ConnectRunnable(strUrl));
                 thread.start();
-
             }
         });
-
     }
 
     class ConnectRunnable implements Runnable {
-        String hostname;
+        String strUrl;
 
-        public ConnectRunnable(String hostname) {
-            this.hostname = hostname;
+        StringBuilder sb = new StringBuilder();
+        public ConnectRunnable(String strUrl) {
+            this.strUrl = strUrl;
         }
 
         @Override
         public void run() {
             try {
-                Socket socket = new Socket(hostname,5001);
+                URL url = new URL(strUrl);
+                // URL 객체가 가진 문자열이 http로 시작하면
+                // openConnection() 호출로 HttpURLConnection 객체를 얻을 수 있음.
+                HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+                if (huc != null) {
+                    huc.setConnectTimeout(10000); // 10초
+                    huc.setRequestMethod("GET"); // "GET" 또는 "POST"
+                    huc.setDoInput(true); // 입력이 가능하게 만듦
+                    huc.setDoOutput(true); // 출력이 가능하게 만듦
 
-                // 출력스트림 준비
-                OutputStream os = socket.getOutputStream();
-                ObjectOutputStream oos = new ObjectOutputStream(os);
+                    // 내부적으로 웹서버에 페이지를 요청하는 과정을 수행함.
+                    int resCode = huc.getResponseCode();
+                   if(resCode == HttpURLConnection.HTTP_OK){
 
-                String strInput = editText2.getText().toString();
-                oos.writeObject(strInput);
-                oos.flush();
+                       // 입력스트림 준비
+                       InputStream is = huc.getInputStream();
+                       InputStreamReader isReader = new InputStreamReader(is);
+                       BufferedReader bReader = new BufferedReader(isReader);
 
-                // 입력스트림 준비
-                InputStream is = socket.getInputStream();
-                ObjectInputStream ois = new ObjectInputStream(is);
-                final String str =  (String) ois.readObject();
+                       String strLine = null;
+                       while(true){
+                           strLine = bReader.readLine();
+                           if(strLine == null){
+                               break;
+                           }
 
-               handler.post(new Runnable() {
-                   @Override
-                   public void run() {
-                       textView.setText(str); // 작업스레드에서 UI객체 접근 불가.(핸들러객체이용)
+                           sb.append(strLine + "\n");
+                       }
+                        bReader.close();
+                        huc.disconnect(); // URLConnection 연결끊기
                    }
-               });
-
-                ois.close();
-                oos.close();
-                socket.close();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            txtMsg.setText(sb.toString());
+                        }
+                    });
+                }
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
+
 }
